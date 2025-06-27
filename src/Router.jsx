ef1b4c2b4750c65642b0180fb26f2c3a26e30715
daeun <rlaekdms901@naver.com>
2025-06-27 16:35:40 +0900

    feat: 참여 완료 라우팅 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SearchPage from './pages/SearchPage';
 import Login from './pages/Login';
-import CreateClub from './pages/CreateClub'
+import CreateClub from './pages/CreateClub';
+import JoinSuccess from './pages/JoinSuccess';
+import Modal from './components/modal/Modal';
 
 export default function AppRouter() {
   return (
@@ -12,6 +14,7 @@ export default function AppRouter() {
         <Route path="/search" element={<SearchPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/create-club" element={<CreateClub />} />
+        <Route path="/join-success" element={<JoinSuccess />} />
       </Routes>
     </BrowserRouter>
   );
