6868b4ff0f40b284ff3dd4aa389edb6a2db9874b
daeun <rlaekdms901@naver.com>
2025-06-28 22:15:37 +0900

    feat: 모달창 api 연결

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,82 +1,148 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import CloseBtn from '../assets/close.svg';
-import Univ from '../assets/gurumton.svg';
 
 import '../styles/modal.css';
 
-export default function Modal({ isOpen, onClose }) {
+const API_URL = 'https://dongari-eum-backend.onrender.com';
+
+export default function Modal({ isOpen, onClose, club }) {
   const [verificationCode, setVerificationCode] = useState(Array(6).fill(''));
   const navigate = useNavigate();
+  const passwordInputs = useRef([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleChange = (index, value) => {
-    if (/^\d?$/.test(value)) {
-      const updateCode = [...verificationCode];
-      updateCode[index] = value;
-      setVerificationCode(updateCode);
+  useEffect(() => {
+    if (isOpen && passwordInputs.current[0]) {
+      passwordInputs.current[0].focus();
+    }
+  }, [isOpen]);
 
-      if (value && index < 5) {
-        document
-          .querySelector(`input[name="verification-${index + 1}"]`)
-          ?.focus();
+  const resetModalState = () => {
+    setVerificationCode(Array(6).fill(''));
+    setErrorMessage('');
+  };
+
+  const handlePasswordChange = (e, index) => {
+    const { value } = e.target;
+    if (/^[0-9]$/.test(value)) {
+      const newPassword = [...verificationCode];
+      newPassword[index] = value;
+      setVerificationCode(newPassword);
+
+      if (index < 5) {
+        passwordInputs.current[index + 1].focus();
       }
+    } else if (value === '') {
+      const newPassword = [...verificationCode];
+      newPassword[index] = '';
+      setVerificationCode(newPassword);
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === 'Backspace' && verificationCode[index] === '' && index > 0) {
+      passwordInputs.current[index - 1].focus();
     }
   };
 
   const isActive = verificationCode.every((digit) => digit !== '');
 
-  const joinButton = () => {
-    navigate('/join-success');
+  const joinButton = async () => {
+    const password = verificationCode.join('');
+    try {
+      await axios.post(`${API_URL}/clubs/join`, {
+        name: club.name,
+        password: password,
+      });
+
+      navigate('/join-success');
+    } catch (error) {
+      const msg = error.response?.data.message || '⚠️ 입장코드가 달라요!';
+      setErrorMessage(msg);
+    }
   };
+
   if (!isOpen) return null;
 
   return (
-    <div className="modal-back" onClick={onClose}>
+    <div
+      className="modal-back"
+      onClick={() => {
+        resetModalState();
+        onClose();
+      }}
+    >
       <div className="modal-in" onClick={(e) => e.stopPropagation()}>
-        <button className="modal-close" onClick={onClose}>
+        <button
+          className="modal-close"
+          onClick={() => {
+            resetModalState();
+            onClose();
+          }}
+        >
           <img src={CloseBtn} alt="close" />
         </button>
         <div>
           <div className="modalTitleWrapper">
-            <img src={Univ} alt="univ" className="univImg" />
+            <img
+              src={`${API_URL}/${club.image_url}`}
+              alt="univ"
+              className="univImg"
+            />
             <div className="title">
               <div>
-                <h1>구름톤 유니브</h1>
-                <span>연합</span>
+                <h1>{club.name}</h1>
+                <span>{club.club_type}</span>
                 <span>IT</span>
               </div>
-              <p>카카오와 구름이 함께하는 전국 대학 IT 연합 동아리</p>
+              <p>{club.description}</p>
             </div>
           </div>
         </div>
-        <div className="codeWrapper">
-          <h3>입장코드</h3>
-          <div className="inputWrapper">
-            {Array.from({ length: 6 }).map((_, index) => (
-              <div key={index}>
-                <input
-                  name={`verification-${index}`}
-                  maxLength={1}
-                  value={verificationCode[index]}
-                  onChange={(e) => handleChange(index, e.target.value)}
-                  type="text"
-                  tabIndex={index + 1}
-                  required
-                />
-              </div>
-            ))}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (isActive) joinButton();
+          }}
+        >
+          <div className="codeWrapper">
+            <h3>입장코드</h3>
+            <div className="inputWrapper">
+              {Array.from({ length: 6 }).map((_, index) => (
+                <div key={index}>
+                  <input
+                    name={`verification-${index}`}
+                    maxLength={1}
+                    value={verificationCode[index]}
+                    onChange={(e) => handlePasswordChange(e, index)}
+                    type="text"
+                    tabIndex={index + 1}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                    ref={(el) => (passwordInputs.current[index] = el)}
+                    required
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="btnWrapper">
-          <button
-            className={`clickBtn ${isActive ? 'active' : 'inactive'}  `}
-            disabled={!isActive}
-            onClick={joinButton}
-          >
-            참여하기
-          </button>
-        </div>
+
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+
+          <div className="btnWrapper">
+            <button
+              type="submit"
+              className={`${
+                isActive ? 'clickBtn active' : 'clickBtn inactive'
+              } ${errorMessage ? 'error-btn' : ''}`}
+              disabled={!isActive}
+            >
+              참여하기
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
